9c3e70d80c61fa54aa6b036350aff640a75d902f
wangjinjian <wjj1252600254@163.com>
2019-09-11 14:53:32 +0800

    springboot-aop

--- mybatisTest/src/main/java/mybatistest/Application.java
+++ mybatisTest/src/main/java/mybatistest/Application.java
@@ -1,9 +1,11 @@
 package mybatistest;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "mybatistest.entity")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
